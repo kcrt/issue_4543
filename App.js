516dc7fed458146590a3150e0a98f1e76bc87326
TAKAHASHI Kyohei <kcrt@kcrt.net>
2022-01-25 23:55:32 +0900

    :beetle: Produce issue #4543

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { NativeBaseProvider, Select, Text } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <NativeBaseProvider>
+    <SafeAreaView>
+      <Text>Select:</Text>
+      <Select>
+        <Select.Item key="1" label="1" value="1" />
+      </Select>
+    </SafeAreaView>
+  </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
